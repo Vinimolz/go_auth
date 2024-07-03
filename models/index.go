ff6fe89218942dca10aaf2f54e131d6944e2e45c
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2024-07-03 11:54:21 -0700

    finished adding models and InitDB function

---
+++ models/index.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	User     string
+	Passwrod string
+	DBName   string
+	SSLMode  string
+}
+
+var DB *gorm.DB
+
+func InitDB(cnf Config) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", 
+							cnf.Host, cnf.User,
+							cnf.Passwrod, cnf.DBName,
+							cnf.DBName, cnf.SSLMode)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Error Opening the databse")
+		panic(err)
+	}
+
+	if err := db.AutoMigrate((&User{})); err != nil {
+		fmt.Println("Error migrating the database")
+		panic(err)
+	}
+
+	fmt.Println("database was migrated")
+
+	DB = db
+}
